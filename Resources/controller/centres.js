3f25ed815d65426ecab46852880b39e861dddf99
Gael Demette PC <demette.gael@gmail.com>
2013-03-11 23:47:30 +0100

    Centres.json + icon

--- Resources/controller/centres.js
+++ Resources/controller/centres.js
@@ -1,7 +1,7 @@
 var win = Titanium.UI.currentWindow;
 function getCentres() {  
     var xhr = Titanium.Network.createHTTPClient();
-    xhr.open("GET","http://poney.spider4all.fr/app_dev.php/ws/centre/index.json");
+    xhr.open("GET","http://poney.spider4all.fr/ws/centres.json");
     xhr.onload = function() {
     	var data = [];
     	var centres = JSON.parse(xhr.responseText);
